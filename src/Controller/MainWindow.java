edc24668defc172d7c45a67a34dbfeea384c53e3
chetanrevappa <dgrchetan@gmail.com>
2020-05-02 19:54:11 +1000

    login+main update 1

--- src/Controller/MainWindow.java
+++ src/Controller/MainWindow.java
@@ -1,14 +1,18 @@
 package Controller;
 
+import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.fxml.FXML;
 import javafx.event.*;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -43,15 +47,29 @@ public class MainWindow implements Initializable {
     @FXML private void jobPost(ActionEvent actionEvent) {
     }
 
-    @FXML private void quitUnilink(ActionEvent actionEvent) {
-
-    }
 
     public void quit(ActionEvent actionEvent) {
-
+        Platform.exit();
     }
 
     public void devInfo(ActionEvent actionEvent) {
+
+        developerInfo.setOnAction((event) -> {
+            FXMLLoader loader = new FXMLLoader();
+            try {
+                loader.setLocation(getClass().getResource("/view/Devinfo.fxml"));
+                Scene scene = new Scene(loader.load());
+                Stage stage = new Stage();
+                stage.setTitle("Developer Information");
+                stage.setScene(scene);
+                stage.show();
+
+
+            } catch (IOException e) {
+                System.out.println("Could not open DevInfo");
+            }
+        });
+
     }
 
 
